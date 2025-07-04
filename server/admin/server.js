17cace7452cc5b781c940422a734a33721f9d42a
Jonah Zimmer <jszimmer545@gmail.com>
2025-07-03 21:03:35 -0500

    update localvar names

--- server/admin/server.js
+++ server/admin/server.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
--- server/bricks/server.js
+++ server/bricks/server.js
@@ -2,9 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const getBricks = async () => {
     const { data: brick, error } = await supabase.from('brick').select('*');
