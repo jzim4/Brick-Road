150c5f0f8d1af4a3ae9a0c3c3a8f45ea1e9146bd
Jonah Zimmer <jzim4@DESKTOP-JVJUGOA.localdomain>
2025-08-01 08:59:51 -0500

    [Fix] update permissions to cors on server

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(cors({
+    origin: [
+        'http://localhost:8080',
+        'https://brick-road.vercel.app'
+    ]
+}));
 app.use(express.json()); // Add this to parse JSON request bodies
 
 app.listen(8000, () => {
